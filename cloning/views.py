9e1a4d995dd4d887cc831c7e9c476fd81295b544
Agnelo <agnelofernandes475@gmail.com>
2022-09-01 19:48:01 +0530

    base.html added

--- cloning/views.py
+++ cloning/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from textwrap import dedent
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'cloning/index.html')
+
+def contact(request):
+    return HttpResponse("This is my contactpage")
+
+def about(request):
+    return HttpResponse("This is my about page")
+
+def privacy(request):
+    return HttpResponse("This is my privacy page")
+
+def disclaimer(request):
+    return HttpResponse("This is my disclaimer page")
